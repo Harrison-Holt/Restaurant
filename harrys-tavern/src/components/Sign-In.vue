1259ef0a3d2237df3f704562d2125c98c0d7f9e1
Harrison-Holt <hholt2901@gmail.com>
2024-06-20 12:23:53 -0400

    third commit

--- harrys-tavern/src/components/Sign-In.vue
+++ harrys-tavern/src/components/Sign-In.vue
@@ -1,17 +1,31 @@
 <template>
-    <section class="d-flex flex-column">
-        <form class="sign-in-form">
-            <label for="email"Email:></label><br>
-            <input id="email" type="email" required><br><br>
-            <label for="passord"Password:></label><br>
-            <input id="password" type="password" required><br><br>
-        </form>
-    </section>
-</template>
-
-<script>
-export default {
-    name: "AppSignin"
-}
-</script>
-
+    <div class="signin">
+      <h2>Sign In</h2>
+      <form>
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="email" id="email" class="form-control" required />
+        </div>
+        <div class="form-group">
+          <label for="password">Password:</label>
+          <input type="password" id="password" class="form-control" required />
+        </div>
+        <button type="submit" class="btn btn-primary">Sign In</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SignIn'
+  };
+  </script>
+  
+  <style scoped>
+  .signin {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 1em;
+  }
+  </style>
+  
